bc3d73e1eba028de2eb12cc4ebb5cf9d7e5a4f52
kaya <kayaskov@hotmail.com>
2019-11-05 22:23:54 +0100

    Java Update

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-var button = document.querySelector('button');
+var button = document.querySelector('.button');
+var target = document.querySelector('body');
 
 button.addEventListener('click', function () {
-    document.body.style.backgroundColor = "black";
-});
+    target.classList.toggle('effect')
+})
+
+//var button = document.querySelector('button');
+//var body = document.querySelector('body');
+
+//button.addEventListener("click", function () {
+//           body.classList.toggle('body2')
+//   }
+
+//button.addEventListener('click', function () {
+//    document.body.style.backgroundColor = "black";
+//});
+
+//declaratie van dom elementen
+//var zoekveld = document.querySelector('body>header');
+//var zoekbutton = document.querySelector('body>header>button');
+
+//actie !
+//zoekbutton.addEventListener("click", function () {
+//    zoekveld.classList.toggle('show-search');
+//});
